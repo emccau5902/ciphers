208ca61e63091ad9f1b118482f4bd1db5ac6cfd0
emccau5902 <23508836+emccau5902@users.noreply.github.com>
2018-02-15 13:45:00 -0500

    Add files via upload

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,16 +1,29 @@
-def shift(letter, n):
-    pass
+def shift(letter, shift_amount):
+    unicode_value = ord(letter) + shift_amount
+    
+    if unicode_value >= 126:
+        unicode_value = 32 + (unicode_value - 126)
+
+    new_letter = chr(unicode_value)
+
+    return new_letter
 
 def encrypt(message, shift_amount):
-    pass
+    encrypt_msg = ''
+
+    for letter in message:
+        encrypt_msg += shift(letter, shift_amount)
+        
+    return encrypt_msg
 
 
 def decrypt(message, shift_amount):
-    pass
+    return encrypt(message, -1 * shift_amount)
 
-secret_message = "encryption is fun"
-encrypted_message = encrypt(secret_message, 3)
+secret_message = "UPPERCASE!!! lowercase. BoTh?"
+encrypted_message = encrypt(secret_message, 70)
+decrypted_message = decrypt(secret_message, 0)
 
+print(secret_message)
 print(encrypted_message)
-
-
+print(decrypted_message)
