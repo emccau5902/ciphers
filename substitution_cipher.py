f9c8b8ae299204f6aa25f1b9587ba565fbd2d39b
emccau5902 <23508836+emccau5902@users.noreply.github.com>
2018-02-15 13:35:45 -0500

    Add files via upload

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -1,15 +1,30 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz "
-key = "qwertyuioplkjhgfdsazxcvbnm "
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ~!@#$%^&*()_+-=`[]{}\|?//><,.:;/'/" 
+key = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM /"' ;:.,<.//?|\}{][`=-+_)(*&^%$#@!'
 
 def encrypt(message):
-    pass
+    encrypt_msg = ""
+
+    for letter in message:
+        loc = alphabet.find(letter)
+        encrypt_msg += key[loc]
+
+    return encrypt_msg
 
 def decrypt(message):
-    pass
+    decrypt_msg = ''
+
+    for letter in message:
+        qr = key.find(letter)
+        decrypt_msg += alphabet[qr]
+
+    return decrypt_msg
 
 
-unencrypted_message = "encryption is fun"
+unencrypted_message = "encryption is fun. UPPERCASE? lowercase!"
 encrypted_message = encrypt(unencrypted_message)
+decrypted_message = decrypt(encrypted_message)
 
 print(unencrypted_message)
+print('')
 print(encrypted_message)
+print(decrypted_message)
